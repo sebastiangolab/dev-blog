196104d5c6a9ba552b5c2f2352baaded0c33ba02
Gołąb Sebastian <sebastian.golab@k5054macbook.local>
2025-01-18 19:01:13 +0100

    Create getSinglePost action

---
+++ src/actions/getSinglePost.ts
@@ -0,0 +1,75 @@
+"use server";
+
+import normalizeSinglePostResponseData from "@/helpers/normalizeSinglePostResponseData";
+import AxiosWP from "@/lib/AxiosWP";
+import {
+  AuthorResponseData,
+  CategoriesResponseData,
+  FeaturedImageResponseData,
+  PostData,
+} from "@/types/postsActionsTypes";
+import axios from "axios";
+
+export type SinglePostResponseData = {
+  data: {
+    post: PostData & {
+      author: AuthorResponseData;
+      categories: CategoriesResponseData;
+      featuredImage: FeaturedImageResponseData;
+    };
+  };
+};
+
+const getSinglePost = async (slug: string): Promise<PostData | null> => {
+  const query = `
+    query GetSinglePost {
+      post(idType: SLUG, id: "${slug}") {
+        id
+        title
+        excerpt
+        content
+        date
+        author {
+          node {
+            name
+          }
+        }
+        categories {
+          edges {
+            node {
+              name
+            }
+          }
+        }
+        featuredImage {
+          node {
+            altText
+            link
+          }
+        }
+      }
+    }
+    `;
+
+  try {
+    const response = await AxiosWP.post<SinglePostResponseData>("/", {
+      query,
+    });
+
+    const responsePostData = response.data.data?.post;
+
+    return responsePostData
+      ? normalizeSinglePostResponseData(responsePostData)
+      : null;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error(error.message);
+    } else {
+      console.error("Something wrong while get single post data");
+    }
+
+    return null;
+  }
+};
+
+export default getSinglePost;
