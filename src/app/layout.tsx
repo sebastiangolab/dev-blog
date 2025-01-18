cb2907d67d86b7746024a354492fd283ecc8b8ad
Gołąb Sebastian <sebastian.golab@k5054macbook.local>
2025-01-18 18:48:55 +0100

    Add metadata to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,14 @@
 import { ReactNode } from "react";
-import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import "@/styles/globals.css";
 import Container from "@/components/Container";
 import Header from "@/components/Header";
+import "@/styles/globals.css";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
   subsets: ["latin"],
   display: "swap",
 });
-
-export const metadata: Metadata = {
-  title: "ImDevBlog",
-  description:
-    "Blog about programming, javascript, react, nextjs, good practices and others.",
-};
 
 export default function RootLayout({
   children,
